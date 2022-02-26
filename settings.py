3cbdc50a6383ab53cb30e82f277f6e5c3fe9eff9
danilevich23555 <danilevich23555@yandex.ru>
2022-02-26 16:11:12 +0300

    end

--- settings.py
+++
@@ -1,8 +0,0 @@
-def tokens():
-    with open('token\\vk_token_communities.txt', 'r') as token_vk_communities:
-        token_vk_communities = token_vk_communities.read().strip()
-    with open('token\\vk_token.txt', 'r') as vk_token:
-        vk_token = vk_token.read().strip()
-        if vk_token == '':
-            vk_token = input('Введите токен для авторизаии, файл vk_token.txt пустой')
-    return [token_vk_communities, vk_token]
